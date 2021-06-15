1f0be8b8fc5bcb294444c49cd9338f70eb23d6c5
phibeck <sophie.beck@mat.ethz.ch>
2021-06-15 10:35:23 -0400

    add more tabs

--- app.py
+++
@@ -1,4 +0,0 @@
-from maindash import app
-
-if __name__ == '__main__':
-    app.run_server(debug=True, port=9375, host='0.0.0.0')
--- tabs/tab2_layout.py
+++
@@ -1,7 +0,0 @@
-import dash_core_components as dcc
-
-def layout(data):
-    return dcc.Tab(
-            label='Fermi surface A(k,0)',
-            children=[]
-            )
