8c0221d4ffcf2b8c58c02928b6b5fccd56e31426
phibeck <sophie.beck@mat.ethz.ch>
2021-06-15 11:25:29 -0400

    Add two more tabs

--- layout.py
+++ layout.py
@@ -5,11 +5,15 @@ import dash_daq as daq
 
 from tabs.tab1_layout import layout as tab1_layout
 from tabs.tab2_layout import layout as tab2_layout
+from tabs.tab3_layout import layout as tab3_layout
+from tabs.tab4_layout import layout as tab4_layout
 
 def layout(data):
     return html.Div([
     dcc.Tabs([
         tab1_layout(data),
-        tab2_layout(data)
+        tab2_layout(data),
+        tab3_layout(data),
+        tab4_layout(data),
         ])
     ])
---
+++ tabs/tab3_layout.py
@@ -0,0 +1,7 @@
+import dash_core_components as dcc
+
+def layout(data):
+    return dcc.Tab(
+            label='Optical spectroscopy',
+            children=[]
+            )
